c6fe7d88f31c0fefa7b70c19ed258c75193a97a2
Silmiuww <nadiasalma078@gmail.com>
2025-11-01 20:03:51 +0700

    Percobaan 2 langkah 1

--- Hewan.java
+++ Hewan.java
@@ -1,23 +1,20 @@
-public class Hewan {
+public abstract class Hewan {
     public double berat;
     public String makanan;
     public String habitat;
 
-    public Hewan(double berat, String makanan, String habitat){
+    public Hewan(double berat, String makanan, String habitat) {
         this.berat = berat;
         this.makanan = makanan;
         this.habitat = habitat;
     }
 
-    public void bergerak(){
-    }
+    public abstract void bergerak();
+    public abstract void bernapas();
 
-    public void bernapas(){
-    }
-
-    public void cetakInfo(){
-        System.out.println("Berat : " + this.berat);
-        System.out.println("Makanan : " + this.makanan);
-        System.out.println("Habitat : " + this.habitat);
+    public void cetakInfo() {
+        System.out.println("Berat    : " + this.berat);
+        System.out.println("Makanan  : " + this.makanan);
+        System.out.println("Habitat  : " + this.habitat);
     }
 }
