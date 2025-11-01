88d4823d44b22491699240a4ced11f90b4b9e785
Silmiuww <nadiasalma078@gmail.com>
2025-11-01 20:04:53 +0700

    Percobaan 2 langkah 4

--- AbstractClassDemo.java
+++ AbstractClassDemo.java
@@ -4,11 +4,5 @@ public class AbstractClassDemo {
         hewan1.cetakInfo();
         hewan1.bergerak();
         hewan1.bernapas();
-        System.out.println();
-
-        Lebah lebah1 = new Lebah("Ratu", 0.05, "Nektar", "Hutan");
-        lebah1.cetakInfo();
-        lebah1.bergerak();
-        lebah1.bernapas();
     }
 }
