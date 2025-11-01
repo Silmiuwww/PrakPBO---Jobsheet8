2ab22a1491b65e78af2c2cbfea86ba139a71a876
Silmiuww <nadiasalma078@gmail.com>
2025-11-01 21:27:43 +0700

    Percobaan 2 langkah 7

--- Lebah.java
+++ Lebah.java
@@ -5,4 +5,18 @@ public class Lebah extends Hewan{
         super(berat, makanan, habitat);
         this.kasta = kasta;
     }
+
+    @Override
+    public void bernapas(){
+        System.out.println("Otot perut mengendur, udara masuk melalui lubang di segmen tubuh");
+        System.out.println("Trakea mengirimkan oksigen");
+        System.out.println("Otot perut berkontraksi, udara dikeluarkan");
+    }
+
+    @Override
+    public void bergerak(){
+        System.out.println("Menggerakkan sayap ke depan");
+        System.out.println("Memutar sayap hampir 90 derajat");
+        System.out.println("Menggerakkan sayap ke belakang");
+    }
 }
